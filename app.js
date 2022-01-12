c97732940f464fd0fb7e82d539c68e0e0f9a1a2b
Shinjiwoooo <yuljeonjww@gmail.com>
2022-01-12 22:24:54 +0900

    2022.10.24

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const canvas = document.getElementById("jsCanvas");
-// console.log(canvas);
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
-ctx.strokeStyle = "#2c2c2c";
+const INITIAL_COLOR = "#2C2C2C";
+const CANVAS_SIZE = 700;
+
+// canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
+// canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.linewidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
   painting = false;
@@ -19,22 +35,75 @@ function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
   if (!painting) {
-    ctx.beginpath();
+    // false 면 moveto  true 면 lineto
+    ctx.beginPath();
     ctx.moveTo(x, y);
   } else {
-    ctx.lineTO(x, y);
+    ctx.lineTo(x, y);
     ctx.stroke();
   }
 }
 
-function onMouseDown(event) {
-  //   console.log(event);
-  painting = true;
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+  console.log(color);
+  // console.log(event.target.style);
+}
+
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.linewidth = size;
+}
+function handleModeClick() {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "paint";
+  }
+}
+
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
+}
+
+function handleCM(event) {
+  event.preventDefault();
+}
+function handleSaveClick() {
+  const image = canvas.toDataURL();
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "PaintJS[EXPORT]";
+  link.click();
 }
 
 if (canvas) {
-  canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", startPainting);
-  canvas.addEventListener("mouseup", stopPainting);
-  canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("mousemove", onMouseMove); // 올리기만했을때
+  canvas.addEventListener("mousedown", startPainting); //눌럿을때
+  canvas.addEventListener("mouseup", stopPainting); // 뗏을때
+  canvas.addEventListener("mouseleave", stopPainting); //범위 벗어날때
+  canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleCM);
+}
+
+Array.from(colors).forEach((color) =>
+  color.addEventListener("click", handleColorClick)
+);
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
 }
